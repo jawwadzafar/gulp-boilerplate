c823d0ad554125de9c531e91b302b69a0de8fded
Jawwad Zafar <jawwad@darwinlabs.io>
2017-08-19 12:56:12 +0530

    updated gulp and bootstrap

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,17 @@
+/*!
+ * SimpleBoilerplate
+ * Simple Gulp Bootstrap BoilerPlate
+ * 
+ * @author Jawwad Zafar
+ * @version 1.0.5
+ * Copyright 2017. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // WRITE YOUR JQUERY HERE
+  });
+
+})(jQuery, window, document);
